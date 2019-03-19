601696c2d33cbf4670d17dcca187732b8ac9c473
Glenn Canavan <gcana001@fiu.edu>
2019-03-19 02:45:53 +0000

    Automatically backed up by Learn

--- current_age_for_birth_year.rb
+++ current_age_for_birth_year.rb
@@ -1,3 +1,5 @@
-def current_age_for_birth_year(1993)
-  2019 - 1993
+def current_age_for_birth_year(1984)
+  2019 - 1984
+age_of_person = current_age_for_birth_year(1984)
+  expect(age_of_person).to eq(19)
 end
